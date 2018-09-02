7047e97c41bf632169268131e53947722e4d02e1
leoharis <leoharis@live.co.uk>
2018-09-02 18:47:47 +0100

    Added random number Added actual request for user input as an integer. Swapped User input and total comparison in if statement Updated output display to reflect changes.

--- simple-maths.py
+++ simple-maths.py
@@ -1,18 +1,24 @@
 """
-Simple Maths v2
+Simple Maths v3
 Author: Leo Haris
 """
+import random
 
 x = 5
 y = 2
 z = 7
-user_input = 9
 
-# calculate total
-total = (x + y) * z
+# Ask user for a number
+print("Enter a number: ")
+user_input = int(input())
+
+# calculate total using a random integer
+total = (random.randint(1, 10) + y) * z
 
 # Is total greater than user input?
-if total > user_input:
-    print("True, total is " + str(total) + ", and user input was " + str(user_input))
+if user_input > total:
+    print("Yes user input is greater than total")
 else:
-    print("False, total is " + str(total) + ", and user input was " + str(user_input))
+    print("No user input is less than total")
+
+print("Total is " + str(total) + ", and user input was " + str(user_input))
